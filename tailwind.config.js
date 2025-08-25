826371b65f9efac8a9c8d2cb01b5881035f9b7b0
Sushil0725 <bhattaraisushil75@gmail.com>
2025-08-25 13:58:14 +0545

    done changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
